225d0e3d90f19fd48c1ac917663e8b5be6c899e8
zhouxuanpo <zhouxuanpo@126.com>
2016-08-03 15:41:00 +0800

    first

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,20 @@ import React, { PropTypes } from 'react'
 
 class Home extends React.Component {
   render () {
+      let styles={
+          root:{
+              width:'100vw',
+              height:'100vh',
+              background:'url("http://pic.qiantucdn.com/58pic/18/25/11/44F58PICJXC_1024.jpg") no-repeat  fixed center center/cover'
+          },
+          h1:{
+              color:'#fff',
+          }
+
+      }
     return(
-      <div>
-        home
+      <div style={styles.root}>
+        <h1 style={styles.h1}>欢迎光临</h1>
       </div>
     )
   }
